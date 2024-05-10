e675e33136afc6ef00e1f2aca287d2557fc769d1
OUSSAMA <58121982+oussama-bosri@users.noreply.github.com>
2024-05-10 01:24:27 +0100

    change font to roboto

--- WebSite/app/layout.js
+++ WebSite/app/layout.js
@@ -1,9 +1,9 @@
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "./_componenets/Header";
 import Footer from "./_componenets/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Roboto({ subsets: ["latin"], weight:"700"});
 
 export const metadata = {
   title: "Create Next App",
